ab010037291f778171b0039af4c5bd64012ad140
lzj <lzj@check.com>
2020-10-12 20:42:32 +0800

    添加 AhoCorasickDoubleArrayTrie<V>

--- csharp/textFilterCompare/textFilterCompare/Program.cs
+++ csharp/textFilterCompare/textFilterCompare/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using AhoCorasick;
+using NReco.Text;
 using ToolGood.Words;
 
 namespace textFilterCompare
@@ -15,9 +17,14 @@ namespace textFilterCompare
             var list = LoadKeywords();
             var text = File.ReadAllText("Talk.txt");
 
+            //WordsSearchSearch(list, text);
+            WordsSearchExSearch(list, text);
+            AhoCorasickDoubleArrayTrieSearch(list, text);
+
+            Console.ReadKey();
         }
 
- 
+
         private static void RegexSearch(List<string> list, string txt)
         {
             list = list.OrderBy(q => q).ToList();
@@ -60,20 +67,59 @@ namespace textFilterCompare
 
         }
 
+        private static void AhoCorasickDoubleArrayTrieSearch(List<string> list, string txt)
+        {
+            var keywords = new Dictionary<string, string>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                keywords[list[i]] = list[i];
+            }
+            var matcher = new AhoCorasickDoubleArrayTrie<string>(keywords);
+            var fs = File.OpenWrite("AhoCorasickDoubleArrayTrie.dat");
+            matcher.Save(fs, true);
+            fs.Close();
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                matcher.ParseText(txt);
+            }
+            watch.Stop();
+            Console.WriteLine(" AhoCorasickDoubleArrayTrie: " + watch.ElapsedMilliseconds.ToString("N0") + "ms");
+        }
+
         private static void WordsSearchSearch(List<string> list, string txt)
         {
             WordsSearch wordsSearch = new WordsSearch();
             wordsSearch.SetKeywords(list);
 
+ 
 
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                wordsSearch.FindAll(txt);
+            }
+            watch.Stop();
+            Console.WriteLine("WordsSearch: " + watch.ElapsedMilliseconds.ToString("N0") + "ms");
         }
 
         private static void WordsSearchExSearch(List<string> list, string txt)
         {
             WordsSearchEx wordsSearch = new WordsSearchEx();
             wordsSearch.SetKeywords(list);
+            wordsSearch.Save("WordsSearchEx.dat");
 
-
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                wordsSearch.FindAll(txt);
+            }
+            watch.Stop();
+            Console.WriteLine("WordsSearchEx: " + watch.ElapsedMilliseconds.ToString("N0") + "ms");
         }
 
 
@@ -88,7 +134,7 @@ namespace textFilterCompare
                 {
                     if (key != string.Empty)
                     {
-                        list.Add(key);
+                        list.Add(key.Split("\t")[0]);
                     }
                     key = sw.ReadLine();
                 }
